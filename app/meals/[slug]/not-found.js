d66b7080f157e91b80fb060645d2f2f6cbf9968e
William <WilliamNyunt@outlook.com>
2024-04-30 20:07:59 +0100

    Meals not found page

---
+++ app/meals/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function MealNotFound() {
+    return (
+        <>
+            <h1>Meal not found</h1>
+            <p>Unfortunately, the meal you are looking for is not found.</p>
+        </>
+    )
+}
